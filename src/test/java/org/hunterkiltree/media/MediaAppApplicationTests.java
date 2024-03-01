8c9f334224377beb498b565d920c583b15ac2e0c
hunterkilltree <snguoila@yahoo.com>
2024-03-01 22:38:05 +0700

    add

---
+++ src/test/java/org/hunterkiltree/media/MediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hunterkiltree.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
